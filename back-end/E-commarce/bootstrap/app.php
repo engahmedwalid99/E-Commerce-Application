ea9d6e027a642379c9807179d84c7eebf1177780
Eng/Ahmed Walid <eng.ahmedwalid69@gmail.com>
2026-08-16 02:58:24 +0300

    Enhance trustProxies configuration with headers
    Updated trustProxies method to include headers for better proxy handling.

--- back-end/E-commarce/bootstrap/app.php
+++ back-end/E-commarce/bootstrap/app.php
@@ -6,6 +6,7 @@ use App\Http\Middleware\User\checkStatus;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -19,7 +20,15 @@ return Application::configure(basePath: dirname(__DIR__))
             'role' => chechRole::class,
             'user-blocked' => blockedUser::class,
         ]);
-        $middleware->trustProxies(at: '*');
+
+        $middleware->trustProxies(
+            at: '*',
+            headers: Request::HEADER_X_FORWARDED_FOR |
+                    Request::HEADER_X_FORWARDED_HOST |
+                    Request::HEADER_X_FORWARDED_PORT |
+                    Request::HEADER_X_FORWARDED_PROTO |
+                    Request::HEADER_X_FORWARDED_AWS_ELB
+        );
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
