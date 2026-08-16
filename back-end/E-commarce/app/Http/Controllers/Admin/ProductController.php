ddb206390a71b9e39123fd41844b8c0be56bcec0
Eng/Ahmed Walid <eng.ahmedwalid69@gmail.com>
2026-08-16 16:05:59 +0300

    Add Controller import for better structure

--- back-end/E-commarce/app/Http/Controllers/Admin/ProductController.php
+++ back-end/E-commarce/app/Http/Controllers/Admin/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\ProductRequest;
 use App\Models\Product;
 use CloudinaryLabs\CloudinaryLaravel\Facades\Cloudinary;
