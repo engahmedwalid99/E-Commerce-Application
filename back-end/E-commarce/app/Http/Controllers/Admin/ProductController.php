ba483593b04805d04f209a612f885e2b9eeecd50
Eng/Ahmed Walid <eng.ahmedwalid69@gmail.com>
2026-08-16 05:45:34 +0300

    Remove unused Controller import in ProductController
    Removed unused import of Controller class.

--- back-end/E-commarce/app/Http/Controllers/Admin/ProductController.php
+++ back-end/E-commarce/app/Http/Controllers/Admin/ProductController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\ProductRequest;
 use App\Models\Product;
 use CloudinaryLabs\CloudinaryLaravel\Facades\Cloudinary;
