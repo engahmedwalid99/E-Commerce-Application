2cf8e4a4211d61faa65e45b11c5832c8144fa398
Eng/Ahmed Walid <eng.ahmedwalid69@gmail.com>
2026-08-16 21:04:42 +0300

    Fix HTML formatting in welcome.blade.php

--- back-end/E-commarce/resources/views/welcome.blade.php
+++ back-end/E-commarce/resources/views/welcome.blade.php
@@ -57,8 +57,8 @@
                 </span>
 
                 <h1 class="font-extrabold leading-[1.3] text-3xl sm:text-4xl md:text-5xl">
-                    كل اللي محتاجه <br>
-                    <span class="text-gold" style"margin:5px 0px;">في مكان واحد</span>
+                    كل اللي محتاجه
+                    <span class="text-gold">في مكان واحد</span>
                 </h1>
 
                 <p class="text-sm sm:text-base leading-relaxed text-[#C7CADA] max-w-md">
