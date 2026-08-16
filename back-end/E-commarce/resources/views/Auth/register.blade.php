2f10f80e9aa3c6303d1e44b89111f70afd05d7e0
Eng/Ahmed Walid <eng.ahmedwalid69@gmail.com>
2026-08-16 22:39:34 +0300

    Update page title to include app name

--- back-end/E-commarce/resources/views/Auth/register.blade.php
+++ back-end/E-commarce/resources/views/Auth/register.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>إنشاء حساب جديد</title>
+    <title>إنشاء حساب جديد | {{ env('APP_NAME') }}</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://www.google.com/recaptcha/api.js" async defer></script>
