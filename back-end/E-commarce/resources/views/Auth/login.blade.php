d84121be920f0d73f65af7ef830e39a99f35db54
Ahmed Walid <eng.ahmedwalid69@gmail.com>
2026-08-18 14:09:25 +0300

    Update Login Page

--- back-end/E-commarce/resources/views/Auth/login.blade.php
+++ back-end/E-commarce/resources/views/Auth/login.blade.php
@@ -213,7 +213,7 @@
                 }, 4000);
             </script>
 
-            <div class="w-full max-w-[400px] my-auto am:mt-[-20px] md:h-[100vh]">
+            <div class="w-full max-w-[400px] my-auto am:mt-[-20px] md:h-[100vh] max-[650px]:mt-[-55px]">
                 <h2 class="font-tajawal font-extrabold text-[28px] m-0 mb-1.5">تسجيل الدخول</h2>
                 <p class="text-muted text-[14.5px] m-0 mb-6">
                     ليس لديك حساب؟
